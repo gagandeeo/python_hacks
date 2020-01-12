2ec24e218642aef9d58bb5fc15171006c8c45924
gagandeeo <48759400+gagandeeo@users.noreply.github.com>
2020-01-12 22:17:21 +0530

    added rest hash type
    and also changed shah224 >> sha224

--- python_hacks/hashg.py
+++ python_hacks/hashg.py
@@ -13,7 +13,7 @@ def value_sha1(hashvalue):
 	hashobj2 = hashlib.sha1()
 	hashobj2.update(hashvalue.encode())
 	return(hashobj2.hexdigest())
-def value_shah224(hashvalue):
+def value_sha224(hashvalue):
 	hashobj3 = hashlib.sha224()
 	hashobj3.update(hashvalue.encode())
 	return(hashobj3.hexdigest())
@@ -92,3 +92,66 @@ def ditto_crypt_url(cryptHash, url):
 		else:
 			print(colored("[-] Password guess " + str(password) + " does not match, trying next....", 'yellow'))
 	print("password not in password list")
+############# 	SHA224 HASHER ##########
+def ditto_sha224_url(sha224hash, url):
+	passlist = str(urlopen(url).read(),'utf-8')
+	for password in passlist.split('\n'):
+		hashguess = hashlib.sha224(bytes(password, 'utf-8')).hexdigest()
+		if hashguess == sha224hash:
+			print(colored("[+] Password is: " + str(password),'green'))
+			return(str(password))
+		else:
+			print(colored("[-] Password guess " + str(password) + " does not match, trying next....", 'yellow'))
+	print("password not in password list")
+def ditto_sha224_list(sha224hash, list):
+	passlist = list
+	for password in passlist:
+		hashguess = hashlib.sha224(bytes(password, 'utf-8')).hexdigest()
+		if hashguess == sha224hash:
+			print(colored("[+] Password is: " + str(password),'green'))
+			return(str(password))
+		else:
+			print(colored("[-] Password guess " + str(password) + " does not match, trying next...", 'yellow'))
+	print("Password not in password list")
+############### SHA256 HASHER ##################
+def ditto_sha256_url(sha256hash, url):
+	passlist = str(urlopen(url).read(),'utf-8')
+	for password in passlist.split('\n'):
+		hashguess = hashlib.sha256(bytes(password, 'utf-8')).hexdigest()
+		if hashguess == sha256hash:
+			print(colored("[+] Password is: " + str(password),'green'))
+			return(str(password))
+		else:
+			print(colored("[-] Password guess " + str(password) + " does not match, trying next....", 'yellow'))
+	print("password not in password list")
+def ditto_sha256_list(sha256hash, list):
+	passlist = list
+	for password in passlist:
+		hashguess = hashlib.sha256(bytes(password, 'utf-8')).hexdigest()
+		if hashguess == sha256hash:
+			print(colored("[+] Password is: " + str(password),'green'))
+			return(str(password))
+		else:
+			print(colored("[-] Password guess " + str(password) + " does not match, trying next...", 'yellow'))
+	print("Password not in password list")
+################### SHA512 HASHER ####################
+def ditto_sha512_url(sha512hash, url):
+	passlist = str(urlopen(url).read(),'utf-8')
+	for password in passlist.split('\n'):
+		hashguess = hashlib.sha512(bytes(password, 'utf-8')).hexdigest()
+		if hashguess == sha512hash:
+			print(colored("[+] Password is: " + str(password),'green'))
+			return(str(password))
+		else:
+			print(colored("[-] Password guess " + str(password) + " does not match, trying next....", 'yellow'))
+	print("password not in password list")
+def ditto_sha512_list(sha512hash, list):
+	passlist = list
+	for password in passlist:
+		hashguess = hashlib.sha512(bytes(password, 'utf-8')).hexdigest()
+		if hashguess == sha512hash:
+			print(colored("[+] Password is: " + str(password),'green'))
+			return(str(password))
+		else:
+			print(colored("[-] Password guess " + str(password) + " does not match, trying next...", 'yellow'))
+	print("Password not in password list")
